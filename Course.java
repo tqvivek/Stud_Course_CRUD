011284b4c7e8d8e4b6cc9f084a09807c5ef62838
tqvivek <122796418+tqvivek@users.noreply.github.com>
2023-03-07 13:33:48 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,48 @@
+package com.crud;
+
+public class Course {
+	private int id;
+	private String name;
+	private float fees;
+
+	public Course() {
+
+	}
+
+	public Course(int id, String name, float fees) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.fees = fees;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getFees() {
+		return fees;
+	}
+
+	public void setFees(float fees) {
+		this.fees = fees;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", fees=" + fees + "]";
+	}
+
+}
